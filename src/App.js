3427135a01f0c3790e9814cd45e20c36f095413f
Youcef <youce.31@gmail.com>
2022-05-26 18:24:42 +0100

    tuto 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
 
+
+//STORE -> GLOBALIZED STATE
+
+//ACTION INCREMENT
+const increment = () => {
+  return { type: 'INCREMENT' }
+}
+const decrement = () => {
+  return { type: 'DECREMENT' }
+}
+
+//REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+}
+
+let store = createStore(counter);
+
+//Display it on the console 
+store.subscribe(() => console.log(store.getState()))
+
+//Dispatch
+
+store.dispatch(increment());  
+store.dispatch(increment());  
+store.dispatch(increment());  
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
